16ef96dae7f996892d8acaed91f59ad8d2e3c96c
daris-coder <dariseshan99@gmail.com>
2025-12-18 16:48:25 +0700

    AKU TAMBAH FUNGSI

--- fungsi.c
+++ fungsi.c
@@ -16,3 +16,30 @@ struct peserta{
 };
 struct peserta data[30];
 int noPeserta = 0;
+
+//TAMBAH PESERTA
+void tambahPeserta(){
+    char lagi;
+    do{
+        system("cls");
+        printf("=== Input Peserta ===\n");
+        printf("Nama : ");
+        scanf(" %[^\n]", data[noPeserta].nama);
+        printf("No HP: ");
+        scanf(" %[^\n]", data[noPeserta].nohp);
+
+        data[noPeserta].sudahBayar = 0;
+        data[noPeserta].lapak = 0;
+        data[noPeserta].beliKantin = 0;
+        data[noPeserta].jumlahIkan = 0;
+        data[noPeserta].beratIkan = 0;
+        noPeserta++;
+
+        printf("Input lagi? (y/n): ");
+        getchar();
+        lagi = getchar();   
+        getchar();
+
+    }while(lagi=='y' || lagi=='Y');
+    }
+    
