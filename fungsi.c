843e00515678613fcdeab5b13850b9f202fb45c2
riofadillah <riofadillah19@student.upi.edu>
2025-12-18 14:58:30 +0700

    nambah struct

--- fungsi.c
+++ fungsi.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "fungsi.h"
 
-void main() {
-printf ("HAio");
-
-for (int i=1; i<=10; i++) {
-printf ("\nNomor urut ke-%d, i");
-i++;
-}
+struct peserta{
+    char nama[30];
+    char nohp[20];
+    int sudahBayar;      
+    int lapak;           
+    int beliKopi;   
+    int beliMie;
+    int jumlahIkan;      
+    int beratIkan;       
+};
+struct peserta data[30];
+int noPeserta = 0;
